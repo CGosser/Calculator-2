d57b397cb84ff38acb9cf4a5874bcf957c9a8e6d
Chris Gosser and hingyi khong <hingyi@>
2018-08-06 14:45:58 -0700

    create bmi calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
-var add = function(number1, number2) {
-	return number1 + number2;
-};
+// var add = function(number1, number2) {
+// 	return number1 + number2;
+// };
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("enter a number:"));
+// var number2 = parseInt(prompt("enter another number:"));
+// var result = multiply(number1, number2);
+// alert(result);
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
+function bmi (weight, height) {
+  return weight/(height*height);
 };
+// alert(bmi(77, 1.778));
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
+var weight = parseFloat(prompt("enter weight"));
+var height = parseFloat(prompt("enter height"));
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
+alert(bmi(weight, height));
